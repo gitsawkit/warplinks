297709f4d4577e5667fb8d528937e187f9f41141
SAWKIT <potions.plaquiste0l@icloud.com>
2025-02-15 01:50:23 +0100

    Datas Links Added

---
+++ src/app/data/links.ts
@@ -0,0 +1,7 @@
+export const links = [
+    { title: "GitHub", href: "https://github.com/gitsawkit" },
+    { title: "LinkedIn", href: "https://www.linkedin.com/in/nicolasjzw/"},
+    { title: "Instagram", href: "https://www.instagram.com/sawkitgram" },
+    { title: "TikTok", href: "https://www.tiktok.com/@sawkitkot"},
+    { title: "Twitch", href: "https://www.twitch.tv/sawkittv"},
+];
