678d2216ed407df8ea6ec7ef2acae386035e8c3e
SAWKIT <potions.plaquiste0l@icloud.com>
2025-02-17 03:24:04 +0100

    Add Profile Data

---
+++ src/app/data/profile.ts
@@ -0,0 +1,6 @@
+export const profile = {
+    firstName: "Nicolas",
+    lastName: "Wieckiewicz",
+    profilePicture: "https://media.licdn.com/dms/image/v2/D4E03AQEggE-xQ9_jUg/profile-displayphoto-shrink_800_800/B4EZT1yn69HUAc-/0/1739290478049?e=1745452800&v=beta&t=IOBz6A0PdbuGVa0YMHOs9Q-g1zs_g8Fz-3Jli3EaKbs",
+    description: "Développeur Full-Stack"
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Card from "./components/Card"
 import { links } from "./data/links"
+import { profile } from "./data/profile";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">WarpLinks</h1>
-      <main className="flex flex-col items-center justify-center bg-gray-900 text-white">
+    <div className="min-h-screen p-6 pt-12 pb-0 xl:p-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col xl:flex-row items-center justify-center bg-secondary">
+        <div>
+          <div className="p-10 gap-4">
+            <Image src={profile.profilePicture} alt={profile.firstName + " " + profile.lastName} width={184} height={184} className="rounded-full border-solid border-2 border-primary"/>
+            <h1 className="text-4xl font-bold pt-5 xl:pt-10">{profile.firstName} {profile.lastName}</h1>
+            <p className="text-xl text-primary">{profile.description}</p>
+          </div>
+        </div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-10">
           {links.map((link, index) => (
             <Card key={index} title={link.title} href={link.href} description={link.description} logoUrl={link.logoUrl} />
